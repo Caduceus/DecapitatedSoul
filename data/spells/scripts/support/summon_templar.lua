2c2483b277dd5f0b20965bc16c2608133e94eb59
Caduceus <decapitated_soul@hotmail.com>
2018-06-20 10:05:13 -0600

    summon templar

--- data/spells/scripts/support/summon_templar.lua
+++ data/spells/scripts/support/summon_templar.lua
@@ -5,10 +5,16 @@ function onCastSpell(player, variant)
             guardian = "templar",
             deSpawn = 2 * 60
             }
-
+            local pos = player:getPosition()
+				if getTilePzInfo(pos) == true then
+					player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You cannot cast this spell in a protection zone!")
+					return false
+				end
+				
                 if #player:getSummons() == 1 then
+						player:getPosition():sendMagicEffect(CONST_ME_POFF)
                         player:sendTextMessage(MESSAGE_INFO_DESCR, "You already have a guardian.")
-                        return false
+                        return false -- true allows casting of spell, false has been producing errors on sendMagicEffect. 
                 end
                 
         local monster = Game.createMonster(config.guardian, config.playerPos)
