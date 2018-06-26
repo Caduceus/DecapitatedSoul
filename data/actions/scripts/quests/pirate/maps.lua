b10b2ef59781681692ed6f349174eb565fd74fa3
Caduceus <decapitated_soul@hotmail.com>
2018-06-25 23:29:28 -0600

    moved map 3

--- data/actions/scripts/quests/pirate/maps.lua
+++ data/actions/scripts/quests/pirate/maps.lua
@@ -1,7 +1,7 @@
 local firePos = {
             [1] = {x=1151, y=1238, z=7, stackpos=2},
             [2] = {x=1194, y=1243, z=7, stackpos=3},
-            [3] = {x=1218, y=1288, z=5, stackpos=1},
+            [3] = {x=1216, y=1290, z=7, stackpos=1},
             [4] = {x=1145, y=1320, z=7, stackpos=3}
         }
 local time_ = 10
@@ -24,7 +24,7 @@ local storageMap4 = 1018
 
 local mapPos1 = Position(1151, 1238, 7)
 local mapPos2 = Position(1194, 1243, 7)
-local mapPos3 = Position(1218, 1288, 5)
+local mapPos3 = Position(1216, 1290, 7)
 local mapPos4 = Position(1145, 1320, 7)
 
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
@@ -57,7 +57,7 @@ if target.uid == 9012 then
 	else
 				Game.createItem(1489, 1, mapPos3)
 				addEvent(removeFire3, time_ * 1000)
-				player:setStorageValue(storageMap3, 1)
+				--player:setStorageValue(storageMap3, 1)
 				player:getPosition():sendMagicEffect(CONST_ME_FIREWORK_RED)
 				player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have burnt the map.")
 	end
