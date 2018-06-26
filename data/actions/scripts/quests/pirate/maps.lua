60941c7b57533c8cf951f5f0df53e606a6c7342b
Caduceus <decapitated_soul@hotmail.com>
2018-06-25 22:51:02 -0600

    burnt map fire event

--- data/actions/scripts/quests/pirate/maps.lua
+++ data/actions/scripts/quests/pirate/maps.lua
@@ -1,3 +1,22 @@
+local firePos = {
+            [1] = {x=1151, y=1238, z=7, stackpos=2},
+            [2] = {x=1194, y=1243, z=7, stackpos=3},
+            [3] = {x=1218, y=1288, z=5, stackpos=1},
+            [4] = {x=1145, y=1320, z=7, stackpos=3}
+        }
+local time_ = 10
+local function removeFire1()
+		doRemoveItem(getThingfromPos(firePos[1]).uid)
+	end
+local function removeFire2()
+		doRemoveItem(getThingfromPos(firePos[2]).uid)
+	end
+local function removeFire3()
+		doRemoveItem(getThingfromPos(firePos[3]).uid)
+	end
+local function removeFire4()
+		doRemoveItem(getThingfromPos(firePos[4]).uid)
+	end
 local storageMap1 = 1015
 local storageMap2 = 1016
 local storageMap3 = 1017
@@ -15,6 +34,7 @@ if target.uid == 9012 then
 		player:sendTextMessage(MESSAGE_INFO_DESCR, "You have already burnt this map.")
 	else
 				Game.createItem(1489, 1, mapPos1)
+				addEvent(removeFire1, time_ * 1000)
 				player:setStorageValue(storageMap1, 1)
 				player:getPosition():sendMagicEffect(CONST_ME_FIREWORK_RED)
 				player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have burnt the map.")
@@ -25,6 +45,7 @@ if target.uid == 9012 then
 		player:sendTextMessage(MESSAGE_INFO_DESCR, "You have already burnt this map.")
 	else
 				Game.createItem(1489, 1, mapPos2)
+				addEvent(removeFire2, time_ * 1000)
 				player:setStorageValue(storageMap2, 1)
 				player:getPosition():sendMagicEffect(CONST_ME_FIREWORK_RED)
 				player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have burnt the map.")
@@ -35,6 +56,7 @@ if target.uid == 9012 then
 		player:sendTextMessage(MESSAGE_INFO_DESCR, "You have already burnt this map.")
 	else
 				Game.createItem(1489, 1, mapPos3)
+				addEvent(removeFire3, time_ * 1000)
 				player:setStorageValue(storageMap3, 1)
 				player:getPosition():sendMagicEffect(CONST_ME_FIREWORK_RED)
 				player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have burnt the map.")
@@ -45,6 +67,7 @@ if target.uid == 9012 then
 		player:sendTextMessage(MESSAGE_INFO_DESCR, "You have already burnt this map.")
 	else
 				Game.createItem(1489, 1, mapPos4)
+				addEvent(removeFire4, time_ * 1000)
 				player:setStorageValue(storageMap4, 1)
 				player:getPosition():sendMagicEffect(CONST_ME_FIREWORK_RED)
 				player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have burnt the map.")
