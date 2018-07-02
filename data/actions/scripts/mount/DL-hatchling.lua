135ebf4448b21aefa37398a55123335ca33cac0c
Caduceus <decapitated_soul@hotmail.com>
2018-07-02 17:32:44 -0600

    updates

--- data/actions/scripts/mount/DL-hatchling.lua
+++ data/actions/scripts/mount/DL-hatchling.lua
@@ -6,7 +6,7 @@ function onUse(player, item, fromPosition, target, toPosition, isHotkey)
     if player:getStorageValue(10034) <= 1 then
         player:sendTextMessage(MESSAGE_INFO_DESCR, "You have received the Dragon Lord Hatchling Mount!")
         player:getPosition():sendMagicEffect(CONST_ME_FIREWORK_RED)
-        player:addMount(34)
+        player:addMount(76)
         player:setStorageValue(10034, 1)
     end
 end
