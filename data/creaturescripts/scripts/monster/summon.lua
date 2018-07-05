99f3123338c1dff1ad3fda3cc1cdb744c0a8c54b
Caduceus <decapitated_soul@hotmail.com>
2018-07-05 12:35:48 -0600

    resolve issue where master is nil

--- data/creaturescripts/scripts/monster/summon.lua
+++ data/creaturescripts/scripts/monster/summon.lua
@@ -1,4 +1,8 @@
 function onThink(creature)
+local master = creature:getMaster()
+	if master == nil then
+	return false
+end
 local hp = creature:getMaster():getMaxHealth()
 local speed = creature:getMaster():getBaseSpeed()
 	if hp > creature:getMaxHealth() then
