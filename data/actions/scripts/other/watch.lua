a3095ec3585885f0d6ed366f1010e51525600047
Caduceus <decapitated_soul@hotmail.com>
2018-07-08 15:35:34 -0600

    updated date and time for clocks

--- data/actions/scripts/other/watch.lua
+++ data/actions/scripts/other/watch.lua
@@ -1,4 +1,4 @@
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	player:sendTextMessage(MESSAGE_INFO_DESCR, "The time is " .. getFormattedWorldTime() .. ".")
+	player:sendTextMessage(MESSAGE_INFO_DESCR, "The current date is " .. os.date("%D") .. ". \nThe time is " .. os.date("%H:%M:%S") .. ".") 
 	return true
 end
