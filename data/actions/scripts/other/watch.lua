788575229eabd09a795d14aacd4b3be8d443c538
Caduceus <decapitated_soul@hotmail.com>
2018-07-08 19:00:15 -0600

    updated strings

--- data/actions/scripts/other/watch.lua
+++ data/actions/scripts/other/watch.lua
@@ -1,4 +1,4 @@
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	player:sendTextMessage(MESSAGE_INFO_DESCR, "The current date is " .. os.date("%D") .. ". \nThe time is " .. os.date("%H:%M:%S") .. ".") 
+	player:sendTextMessage(MESSAGE_INFO_DESCR, "The current day is " .. os.date("%A %B %d, %Y") .. ". \nThe time is " .. os.date("%X") .. ".") 
 	return true
 end
