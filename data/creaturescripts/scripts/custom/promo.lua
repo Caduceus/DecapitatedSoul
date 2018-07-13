f9647dbb4b2f3c909014dfd28ec90dcddfb4a2e3
Caduceus <decapitated_soul@hotmail.com>
2018-07-12 21:22:03 -0600

    promo updated for existing accounts only

--- data/creaturescripts/scripts/custom/promo.lua
+++ data/creaturescripts/scripts/custom/promo.lua
@@ -1,12 +1,12 @@
 function onLogin(player)
-	if player:getAccountStorageValue(accountStorage.returnPlayerPromo) == 1 then
+	if player:getAccountStorageValue(accountStorage.returnPlayerPromo) == -1 or player:getAccountStorageValue(accountStorage.returnPlayerPromo) == 2 then
 	return true
 end
-	if player:getAccountStorageValue(accountStorage.returnPlayerPromo) ~= 1 then
+	if player:getAccountStorageValue(accountStorage.returnPlayerPromo) == 1 then
 		player:popupFYI("  Welcome back to Decapitated Soul, " .. player:getName() .. "! We have been hard at work creating new content for you to try out! As a token of our appreciation for your return, we have given you two free VIP days and a Tibia Coin. Are you ready for all that awaits?")
 		player:addVipDays(2)
 		player:addItem(24774, 1)
-		player:setAccountStorageValue(accountStorage.returnPlayerPromo, 1)
+		player:setAccountStorageValue(accountStorage.returnPlayerPromo, 2)
 	return true
 	end
 end
