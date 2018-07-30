5c8f4243c760137a60625235f411bbac9b68af19
Caduceus <decapitated_soul@hotmail.com>
2018-07-29 19:28:16 -0600

    baking

--- data/actions/scripts/other/baking.lua
+++ data/actions/scripts/other/baking.lua
@@ -1,6 +1,6 @@
 local liquidContainers = {1775, 2005, 2006, 2007, 2008, 2009, 2011, 2012, 2013, 2014, 2015, 2023, 2031, 2032, 2033}
 local millstones = {1381, 1382, 1383, 1384}
-local dough = {6277, 8846}
+local ovens = {1786, 6356, 6358, 6360, 6362}
 
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 	if item.itemid == 2692 and isInArray(liquidContainers, target.itemid) then
@@ -13,14 +13,19 @@ function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 			player:addItem(6277, 1)
 			target:transform(target.itemid, 0)
 		end
-	elseif isInArray(dough, item.itemid) then
-		if target.itemid == 1786 then
-			item:transform(item.itemid + 1)
+	elseif item.itemid == 8846 and isInArray(ovens, target.itemid) then
+			item:transform(8847)
 		elseif target.itemid == 6574 then
 			item:transform(8846)
 			target:remove()
+	elseif item.itemid == 6277 and isInArray(ovens, target.itemid) then
+	local cakeCookie = math.random(1, 10)
+		if cakeCookie >= 5 then
+			item:transform(2687, math.random(1, 12)) 
+		else
+			item:transform(6278)
 		end
-	elseif item.itemid == 2693 and target.itemid == 1786 then
+	elseif item.itemid == 2693 and isInArray(ovens, target.itemid) then
 		item:transform(2689)
 	elseif isInArray(millstones, target.itemid) then
 		item:transform(item.itemid, item.type - 1)
