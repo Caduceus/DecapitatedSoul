68c204c0103787eb8ddf8b60f3935421c6ca48cc
Caduceus <decapitated_soul@hotmail.com>
2018-08-04 19:05:13 -0600

    secret entrance update

--- data/actions/scripts/custom/Lilith_Piano.lua
+++ data/actions/scripts/custom/Lilith_Piano.lua
@@ -1,6 +1,8 @@
   local storage = 15001
   local wallPos = {
-            [1] = {x=1024, y=926, z=9, stackpos=2}
+            [1] = {x=1024, y=926, z=9, stackpos=2},
+            [2] = {x=1024, y=927, z=9, stackpos=3},
+            [3] = {x=1023, y=927, z=9, stackpos=3}
         }
 
         local time_ = 5 -- 5 seconds
@@ -8,9 +10,11 @@
         function onUse(player, item, fromPosition, target, toPosition, isHotkey)
             local function reset()
                  -- reset switch
-            item:transform(2080)
+            local piano = Game.createItem(2080, 1, wallPos[2])
                 -- reset walls
+					doRemoveItem(getThingfromPos(wallPos[3]).uid)
                     Game.createItem(9119, 1, wallPos[1])
+                    piano:setActionId(15108)
                 end
                 
                 -- check if lever is currently used
@@ -19,21 +23,17 @@
 				doSendMagicEffect(pos, CONST_ME_POOF)
                 return false
              end
-            -- If door is open, then can not reuse again while open.
-             if item.itemid == 2082 then
-                player:sendTextMessage(MESSAGE_INFO_DESCR, "It Will Not Budge.")
-                return false
-                end
 
             -- add reset
-            item:transform(2082)
             addEvent(reset, time_ * 1000)
    
             -- remove walls, send message
             for i = 1, #wallPos do
             item:getPosition():sendMagicEffect(CONST_ME_SOUND_BLUE)
+				doRemoveItem(getThingfromPos(wallPos[2]).uid)
                 doRemoveItem(getThingfromPos(wallPos[i]).uid)
             end
+				Game.createItem(2080, 1, wallPos[3])
             
             player:sendTextMessage(MESSAGE_INFO_DESCR, "Hurry, the door is closing!")
             return true
--- data/globalevents/scripts/text.lua
+++ data/globalevents/scripts/text.lua
@@ -21,7 +21,6 @@ local effects = {
 	{position = Position(1078, 1197, 7), text = '', effect = CONST_ME_FIREATTACK}, --Demon Oak
 	{position = Position(1085, 1194, 7), text = '', effect = CONST_ME_FIREATTACK}, --Demon Oak
 	{position = Position(1019, 935, 8), text = '', effect = CONST_ME_STUN}, -- Drogheda Temple Spawn
-	{position = Position(1023, 927, 9), text = '', effect = CONST_ME_SOUND_WHITE}, -- Piano (Lilith's Chamber)
 	{position = Position(705, 708, 7), text = 'Demon Helmet Quest'}, --Demon Helmet quest
 	{position = Position(704, 708, 7), text = '', effect = CONST_ME_FIREATTACK}, --Demon Helmet quest
 	{position = Position(706, 708, 7), text = '', effect = CONST_ME_FIREATTACK}, --Demon Helmet quest
