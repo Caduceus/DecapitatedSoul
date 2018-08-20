f2c04f6989914773cd95b9c77843adfd4bfbafc8
Caduceus <decapitated_soul@hotmail.com>
2018-08-20 14:53:35 -0600

    caduceus greeting

--- data/creaturescripts/scripts/firstitems.lua
+++ data/creaturescripts/scripts/firstitems.lua
@@ -2,6 +2,10 @@ local firstItems = {2120, 2132, 2175, 2461, 2649, 2643, 2382, 20624}
 
 function onLogin(player)
 
+	local function admin()
+		sendChannelMessage(3, TALKTYPE_CHANNEL_R1, "Caduceus † 999 † [Dev]: Welcome to Decapitated Soul, " .. player:getName() .. "!")
+	end
+	
 	if player:getLastLoginSaved() == 0 then
 	
 		if player:getVocation():getBase():getId() == 1 then
@@ -17,7 +21,7 @@ function onLogin(player)
 		player:addItem(player:getSex() == 0 and 2651 or 2650, 1)
 		player:addItem(3960, 1):addItem(2671, 10)
 		player:openChannel(3)
-		player:setDirection(DIRECTION_SOUTH)
+		player:setDirection(DIRECTION_WEST)
 		player:popupFYI("Welcome to Decapitated Soul, " .. player:getName() .. "! Basic info can be found in your READ ME item, equipt in your ring slot. If you have any questions, please use our World Chat. Enjoy your Free Blessing until level 80!")
 		player:loadVipData()
 		player:updateVipTime()
@@ -25,6 +29,7 @@ function onLogin(player)
 		player:setStorageValue(9999, player:getVipDays())
 		player:save()
 		addEvent(broadcastMessage, 15 * 1000, "Welcome Our Newest Player: " .. player:getName() .. "", MESSAGE_STATUS_CONSOLE_ORANGE)
+		addEvent(admin, 60 * 1000)
 	end
 	return true
 end
