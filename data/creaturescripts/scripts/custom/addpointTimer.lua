625343f41554961cb2ec680c3391ee04c037d748
Caduceus <decapitated_soul@hotmail.com>
2018-08-30 11:08:51 -0600

    bonus coin

--- data/creaturescripts/scripts/custom/addpointTimer.lua
+++ data/creaturescripts/scripts/custom/addpointTimer.lua
@@ -17,13 +17,24 @@ end
  
 	local playerName = result.getString(resultId, "playerName")
 	local logs = string.format("" ..creature:getName().. " gained 1 coin", logs, playerName)
+	local logsBonus = string.format("" ..creature:getName().. " gained 1 coin and 1 tibia coin", logsBonus, playerName)
 	
     if creature:getStorageValue(455577) == os.time() then
-		db.query("UPDATE `accounts` SET `coins` = `coins` + " .. count .. " WHERE `id` = " .. creature:getAccountId() .. ";")
-		creature:sendTextMessage(MESSAGE_EVENT_ORANGE, "You gained " .. count .. " coin for being online for 1 hour. Your new coin balance is ".. creature:getCoinsBalance() ..".")
-		creature:setStorageValue(455577, os.time() + time)
-		creature:save()
-		doWriteLogFile(file, logs)
-		print(">> Coin awarded: " .. creature:getName() .. " recieved " .. count .. " coin on " .. os.date("%m/%d/%Y at %H:%M:%S") .. ".")
+		if math.random(10) == 10 then
+			db.query("UPDATE `accounts` SET `coins` = `coins` + " .. count .. " WHERE `id` = " .. creature:getAccountId() .. ";")
+			creature:sendTextMessage(MESSAGE_EVENT_ORANGE, "You gained " .. count .. " coin for being online for 1 hour. Your new coin balance is ".. creature:getCoinsBalance() ..".")
+			creature:setStorageValue(455577, os.time() + time)
+			creature:addItem(24774, 1)
+			creature:save()
+			doWriteLogFile(file, logsBonus)
+			print(">> Coin and bonus awarded: " .. creature:getName() .. " recieved " .. count .. " coin and 1 tibia coin on " .. os.date("%m/%d/%Y at %H:%M:%S") .. ".")
+		elseif math.random(10) <= 9 then
+			db.query("UPDATE `accounts` SET `coins` = `coins` + " .. count .. " WHERE `id` = " .. creature:getAccountId() .. ";")
+			creature:sendTextMessage(MESSAGE_EVENT_ORANGE, "You gained " .. count .. " coin for being online for 1 hour. Your new coin balance is ".. creature:getCoinsBalance() ..".")
+			creature:setStorageValue(455577, os.time() + time)
+			creature:save()
+			doWriteLogFile(file, logs)
+			print(">> Coin awarded: " .. creature:getName() .. " recieved " .. count .. " coin on " .. os.date("%m/%d/%Y at %H:%M:%S") .. ".")
+		end
     end
 end
