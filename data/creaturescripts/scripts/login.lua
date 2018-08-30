ee6d607ca5884bddde4f9907a51b786c3e356000
Caduceus <decapitated_soul@hotmail.com>
2018-08-30 12:34:29 -0600

    coin timer reset

--- data/creaturescripts/scripts/login.lua
+++ data/creaturescripts/scripts/login.lua
@@ -72,10 +72,15 @@ setPlayerStorageValue(player, estr, -1)
 		stats.playerId = player:getId()
 	end
 	
+	local lastLogout = player:getLastLogout()
+	local offlineTime = lastLogout ~= 0 and math.min(os.time() - lastLogout, 86400 * 21) or 0
 	local time = 1*60*60 -- 1s*60s = 60s * 60s = 60min
 	if player:getAccountId() == 1 or player:getAccountId() == 2 then
 		player:setStorageValue(455577, -1)
 		player:sendTextMessage(MESSAGE_EVENT_ORANGE, "[Admin Account]: Your current coin balance is ".. player:getCoinsBalance() ..".")
+	elseif offlineTime < 300 then
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Your coin timer did not reset! Try !coin to see your time remaining.")
+		return true
 	else
 		player:setStorageValue(455577, os.time() + time)
 		player:save()
--- data/creaturescripts/scripts/logout.lua
+++ data/creaturescripts/scripts/logout.lua
@@ -1,11 +1,10 @@
 function onLogout(player)
 	player:getPosition():sendMagicEffect(CONST_ME_EXPLOSIONHIT)
-	player:setStorageValue(455577, -1)
 	local playerId = player:getId()
 	if nextUseStaminaTime[playerId] ~= nil then
 		nextUseStaminaTime[playerId] = nil
 	end
-
+	
 	local stats = player:inBossFight()
 	if stats then
 		-- Player logged out (or died) in the middle of a boss fight, store his damageOut and stamina
