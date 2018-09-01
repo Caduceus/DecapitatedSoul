a60a64e9ec180e17ef4c8b03d0c3b95fdf85c4b1
Caduceus <decapitated_soul@hotmail.com>
2018-09-01 11:05:19 -0600

    first login storage set

--- data/creaturescripts/scripts/custom/startCoinTimer.lua
+++ data/creaturescripts/scripts/custom/startCoinTimer.lua
@@ -5,8 +5,13 @@ local time = 1*60*60 -- 1s*60s = 60s * 60s = 60min
 	if player:getAccountId() == 1 or player:getAccountId() == 2 then
 		player:setStorageValue(455577, -1)
 		player:sendTextMessage(MESSAGE_EVENT_ORANGE, "[Admin Account]: Your current coin balance is ".. player:getCoinsBalance() ..".")
+	elseif player:getLastLoginSaved() <= 0 then
+		player:setStorageValue(455577, os.time() + time)
+		player:save()
+		player:sendTextMessage(MESSAGE_EVENT_ORANGE, "For every 1 hour online, you will receive 1 coin to use in the store. Your current coin balance is ".. player:getCoinsBalance() ..".")
 	elseif offlineTime < 300 then
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Your coin timer did not reset! Use command !coin to see your time remaining.")
+		player:sendTextMessage(MESSAGE_EVENT_ORANGE, "Your current coin balance is ".. player:getCoinsBalance() ..".")
 	else
 		player:setStorageValue(455577, os.time() + time)
 		player:save()
