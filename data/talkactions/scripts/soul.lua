5c97971305563ab8d1ab401709ecec7ca541e34d
Caduceus <decapitated_soul@hotmail.com>
2018-09-02 18:50:35 -0600

    soul add/remove talkaction

---
+++ data/talkactions/scripts/soul.lua
@@ -0,0 +1,27 @@
+function onSay(player, words, param)
+	if not player:getGroup():getAccess() then
+		return true
+	end
+
+	if player:getAccountType() < ACCOUNT_TYPE_GOD then
+		return false
+	end
+	
+	local targetPlayer = Player(param)
+	if targetPlayer == nil then
+			player:sendCancelMessage('Usage: ' .. words .. ' <playername>')
+			return true
+		end	
+    
+	local total = targetPlayer:getSoul()
+	local max = targetPlayer:getMaxSoul()
+	local action = string.sub(words, 2, #words) .. 'ed'
+ 
+   if words == '/removesoul' then
+        targetPlayer:addSoul(-total)
+        player:sendCancelMessage('You have removed ' .. total .. ' soul from ' .. targetPlayer:getName() .. '.')
+    elseif words == '/addsoul' then
+        targetPlayer:addSoul(max)
+        player:sendCancelMessage('You have added ' .. max .. ' soul to ' .. targetPlayer:getName() .. '.')
+    end
+end
