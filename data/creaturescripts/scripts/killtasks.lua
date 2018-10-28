2421ffe7eb13c8cb789777be344b55f535dcda88
Caduceus <decapitated_soul@hotmail.com>
2018-10-27 22:47:01 -0400

    killtask cleanup

--- data/creaturescripts/scripts/killtasks.lua
+++ data/creaturescripts/scripts/killtasks.lua
@@ -39,27 +39,27 @@ function onKill(player, target)
 end
 
 local monsters = {
-	 ['rotworm'] = {amount = 50, storage = 19000, startstorage = 5010, startvalue = 1},
-	 ['carrion worm'] = {amount = 50, storage = 19001, startstorage = 5011, startvalue = 1},
-	 ['amazon'] = {amount = 50, storage = 19002, startstorage = 5012, startvalue = 1},
-	 ['cyclops'] = {amount = 50, storage = 19003, startstorage = 5013, startvalue = 1},
-	 ['dragon hatchling'] = {amount = 50, storage = 19004, startstorage = 5014, startvalue = 1},
-	 ['dragon lord hatchling'] = {amount = 50, storage = 19004, startstorage = 5014, startvalue = 1},
-	 ['hero'] = {amount = 50, storage = 19005, startstorage = 5015, startvalue = 1},
-     ['dragon'] = {amount = 50, storage = 19006, startstorage = 5016, startvalue = 1},
-     ['dragon lord'] = {amount = 50, storage = 19007, startstorage = 5017, startvalue = 1},
-     ['demodras'] = {amount = 50, storage = 19008, startstorage = 5018, startvalue = 1},
-     ['hydra'] = {amount = 50, storage = 19009, startstorage = 5019, startvalue = 1},
-     ['demon'] = {amount = 50, storage = 19010, startstorage = 5020, startvalue = 1},
-     ['warlock'] = {amount = 50, storage = 19011, startstorage = 5021, startvalue = 1},
-     ['orshabaal'] = {amount = 50, storage = 19012, startstorage = 5022, startvalue = 1},
-     ['lost soul'] = {amount = 50, storage = 19013, startstorage = 5023, startvalue = 1},
-     ['chocobo warrior'] = {amount = 50, storage = 19014, startstorage = 5024, startvalue = 1},
-     ['ghastly dragon'] = {amount = 50, storage = 19015, startstorage = 5025, startvalue = 1},
-     ['frost dragon'] = {amount = 50, storage = 19016, startstorage = 5026, startvalue = 1},
-     ['grim reaper'] = {amount = 50, storage = 19017, startstorage = 5027, startvalue = 1},
-     ['juggernaut'] = {amount = 50, storage = 19018, startstorage = 5028, startvalue = 1},
-     ['vampire'] = {amount = 50, storage = 19019, startstorage = 5029, startvalue = 1}
+	 ['rotworm'] = {storage = 19000, startstorage = 5010},
+	 ['carrion worm'] = {storage = 19001, startstorage = 5011},
+	 ['amazon'] = {storage = 19002, startstorage = 5012},
+	 ['cyclops'] = {storage = 19003, startstorage = 5013},
+	 ['dragon hatchling'] = {storage = 19004, startstorage = 5014},
+	 ['dragon lord hatchling'] = {storage = 19004, startstorage = 5014},
+	 ['hero'] = {storage = 19005, startstorage = 5015},
+     ['dragon'] = {storage = 19006, startstorage = 5016},
+     ['dragon lord'] = {storage = 19007, startstorage = 5017},
+     ['demodras'] = {storage = 19008, startstorage = 5018},
+     ['hydra'] = {storage = 19009, startstorage = 5019},
+     ['demon'] = {storage = 19010, startstorage = 5020},
+     ['warlock'] = {storage = 19011, startstorage = 5021},
+     ['orshabaal'] = {storage = 19012, startstorage = 5022},
+     ['lost soul'] = {storage = 19013, startstorage = 5023},
+     ['chocobo warrior'] = {storage = 19014, startstorage = 5024},
+     ['ghastly dragon'] = {storage = 19015, startstorage = 5025},
+     ['frost dragon'] = {storage = 19016, startstorage = 5026},
+     ['grim reaper'] = {storage = 19017, startstorage = 5027},
+     ['juggernaut'] = {storage = 19018, startstorage = 5028},
+     ['vampire'] = {storage = 19019, startstorage = 5029}
 }
 function onThink(player)
 	if player:getStorageValue(62004) == -1 then
