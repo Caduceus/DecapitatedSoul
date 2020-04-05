9d590ed3bd976df2cc18535237f6ae607b97ef8e
Caduceus <decapitated_soul@hotmail.com>
2020-04-05 08:53:16 -0700

    Update magiceffect.lua

--- data/talkactions/scripts/magiceffect.lua
+++ data/talkactions/scripts/magiceffect.lua
@@ -3,6 +3,26 @@ function onSay(player, words, param)
 		return true
 	end
 
-	player:getPosition():sendMagicEffect(tonumber(param))
+	local t = param:split(",")
+	local effectType = tonumber(t[1])
+     if not effectType then
+        return false
+    end
+         
+    if effectType >= 0 
+		and (effectType <= 77 or effectType >= 157)
+		and (effectType <= 159 or effectType >= 166)
+		and effectType ~= 174
+		and effectType ~= 183
+		and (effectType <= 187 or effectType >= 190)
+		and effectType ~= 192
+		and effectType ~= 200
+		and effectType <= 206 then
+	
+	
+		player:getPosition():sendMagicEffect(tonumber(param))
+	else
+		player:sendCancelMessage("An effect with that number does not exist.")
+	end
 	return false
 end
