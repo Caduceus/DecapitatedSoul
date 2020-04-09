925f3f9573c4121047b977d8fe00d3941f0643ea
Caduceus <decapitated_soul@hotmail.com>
2020-04-09 16:00:18 -0700

    Demon Oak Flame Spot Fix

--- data/movements/scripts/quests/demon oak/squares.lua
+++ data/movements/scripts/quests/demon oak/squares.lua
@@ -14,11 +14,18 @@ function onStepIn(creature, item, position, fromPosition)
 	if not player then
 		return true
 	end
+	
+	if player:getStorageValue(Storage.DemonOak.Squares) == 5 then
+		player:sendTextMessage(MESSAGE_STATUS_SMALL, "Flame spot complete.")
+		return false
+	end
 
 	local status = math.max(player:getStorageValue(Storage.DemonOak.Squares), 0)
 	if item.uid - startUid == status + 1 then
 		player:setStorageValue(Storage.DemonOak.Squares, status + 1)
+		player:save()
 		player:getPosition():sendMagicEffect(CONST_ME_FIREWORK_RED)
+		player:sendTextMessage(MESSAGE_STATUS_SMALL, "Flame spot " .. player:getStorageValue(Storage.DemonOak.Squares) .."/5.")
 		player:say(voices[math.random(#voices)], TALKTYPE_MONSTER_YELL, false, player, DEMON_OAK_POSITION)
 	end
 	return true
